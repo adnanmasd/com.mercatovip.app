9dff6853ba69196490a0b121dc77eeab29feea57
adnanmasd <adnan.masd@gmail.com>
2019-04-21 16:55:32 +0300

    2.2.1 Post Android Release(2.2.0)

--- src/main.js
+++ src/main.js
@@ -88,7 +88,7 @@ let appinstance = new Vue({
   framework7: {
     id: 'com.mercatovip.app',
     name: 'MercatoVIP',
-    version : '2.2.0',
+    version : '2.2.1',
     theme : 'auto', // md or ios
     touch: {
       tapHold: true,
